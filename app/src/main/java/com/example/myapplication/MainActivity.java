0761aa73216d3fb827d85ca1dbebc4fba388cbeb
Kevin <kevin.sang.yu@gmail.com>
2021-05-27 20:40:58 +1000

    Basics with buttons and TextView, setting disabled, changing text and outputting to log.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
+import android.util.Log;
 import android.os.Bundle;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("appStartup", "app launched.");
+    }
+
+    public void disable(View v) {
+        v.setEnabled(false);
+        Log.d("success", "Button was disabled.");
+        buttonTextChange(v);
+        changeText();
+    }
+
+    public void buttonTextChange(View v) { // how do we pass in parameters? for example a string to change text to
+        Button b = (Button) v;
+        b.setText("Disabled");
+        Log.d("success", "Button was renamed");
+    }
+
+    public void changeText() {
+        TextView v = findViewById(R.id.hello); // is static at the moment
+        v.setText("Changed text");
+        // ((TextView)findViewById(R.id.hello)).setText("Changed text"); also works
+        Log.d("success", "hello text was changed");
     }
 }
